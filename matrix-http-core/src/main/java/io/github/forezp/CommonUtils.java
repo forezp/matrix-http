dd0c791c03e7cf40a2defa62b45a75274436cb42
forezp <124746406@qq.com>
2020-01-04 23:54:49 +0800

    1

--- matrix-http-core/src/main/java/io/github/forezp/CommonUtils.java
+++ matrix-http-core/src/main/java/io/github/forezp/CommonUtils.java
@@ -3,7 +3,8 @@ package io.github.forezp;
 public class CommonUtils {
 
     public static String decorateUrl(String url) {
-        if (!url.startsWith("http://")) {
+        url = url.trim();
+        if (!url.startsWith("http://") && !url.startsWith("https://")) {
             url = "http://" + url;
         }
         return url;
